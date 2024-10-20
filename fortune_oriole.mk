3f2e79d9760956988f68c52bc135d667d040212e
Ramadhani <ramadhanifals@gmail.com>
2024-10-20 06:46:28 +0000

    raviole: Initialized for FortuneOS
    Change-Id: I4a79db8ef675f43cf0c2302d50bed751307eb9cf

--- fortune_oriole.mk
+++ fortune_oriole.mk
@@ -6,28 +6,29 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/fortune/config/common_full_phone.mk)
 
 # Inherit device configuration
-DEVICE_CODENAME := raven
+DEVICE_CODENAME := oriole
 DEVICE_PATH := device/google/raviole
-VENDOR_PATH := vendor/google/raven
+VENDOR_PATH := vendor/google/oriole
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-$(call inherit-product, device/google/gs101/lineage_common.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+$(call inherit-product, device/google/gs101/fortune_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-fortune.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 6 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_MODEL := Pixel 6
+PRODUCT_NAME := fortune_$(DEVICE_CODENAME)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="raven-user 15 AP3A.241005.015 12366759 release-keys" \
-    BuildFingerprint=google/raven/raven:15/AP3A.241005.015/12366759:user/release-keys \
+    BuildDesc="oriole-user 15 AP3A.241005.015 12366759 release-keys" \
+    BuildFingerprint=google/oriole/oriole:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
